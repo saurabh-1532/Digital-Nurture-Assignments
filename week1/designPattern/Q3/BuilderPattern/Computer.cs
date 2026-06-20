cee205c1b86849c4683a618d6c321f228e2069da
saurabh-1532 <saurabh2002153@gmail.com>
2026-06-20 23:20:42 +0530

    adding q3 Builder pattern

---
+++ week1/designPattern/Q3/BuilderPattern/Computer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Computer
+{
+    public string Processor { get; private set; }
+    public string Memory { get; private set; }
+    public string Storage { get; private set; }
+    public bool HasGraphicsCard { get; private set; }
+
+    private Computer(SystemBuilder builder)
+    {
+        Processor = builder.Processor;
+        Memory = builder.Memory;
+        Storage = builder.Storage;
+        HasGraphicsCard = builder.HasGraphicsCard;
+    }
+
+    public void DisplaySpecs()
+    {
+        Console.WriteLine($"CPU: {Processor}");
+        Console.WriteLine($"RAM: {Memory}");
+        Console.WriteLine($"Storage: {Storage}");
+        Console.WriteLine($"Dedicated GPU: {HasGraphicsCard}");
+        Console.WriteLine("-------------------------");
+    }
+
+    public class SystemBuilder
+    {
+        public string Processor { get; private set; }
+        public string Memory { get; private set; }
+        public string Storage { get; private set; }
+        public bool HasGraphicsCard { get; private set; }
+
+        public SystemBuilder(string processor, string memory)
+        {
+            Processor = processor;
+            Memory = memory;
+        }
+
+        public SystemBuilder AddStorage(string storage)
+        {
+            Storage = storage;
+            return this;
+        }
+
+        public SystemBuilder IncludeGPU(bool hasGpu)
+        {
+            HasGraphicsCard = hasGpu;
+            return this;
+        }
+
+        public Computer BuildSystem()
+        {
+            return new Computer(this);
+        }
+    }
+}
---
+++ week1/designPattern/Q3/BuilderPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Building Basic Office Desktop...");
+
+        Computer officePC = new Computer.SystemBuilder("Intel Core i5", "16GB")
+            .AddStorage("512GB SSD")
+            .IncludeGPU(false)
+            .BuildSystem();
+
+        officePC.DisplaySpecs();
+
+        Console.WriteLine("Building High-End Gaming Rig...");
+
+        Computer gamingPC = new Computer.SystemBuilder("AMD Ryzen 9", "32GB")
+            .AddStorage("2TB NVMe SSD")
+            .IncludeGPU(true)
+            .BuildSystem();
+
+        gamingPC.DisplaySpecs();
+    }
+}
